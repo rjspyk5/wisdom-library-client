897633339ef16b24994694358edcfc4ee99e3c0d
rjspyk5 <rjspyk5@gmail.com>
2024-05-12 01:37:57 +0600

    add catagories section

--- src/Pages/Home/CatagoriesSection/Catagories.jsx
+++ src/Pages/Home/CatagoriesSection/Catagories.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CatagoriesCard } from "./CatagoriesCard";
+import { useAxiosSequre } from "../../../Hooks/useAxiosSecure";
+import axios from "axios";
 
 export const Catagories = () => {
+  const [catagoryName, setcatagoryName] = useState([]);
+  const axiosSequre = useAxiosSequre();
+  useEffect(() => {
+    axiosSequre.get("/categories").then((res) => setcatagoryName(res.data));
+  }, []);
+
   return (
-    <div className="grid grid-cols-4 gap-6 ">
-      <CatagoriesCard />
-      <CatagoriesCard />
-      <CatagoriesCard />
-      <CatagoriesCard />
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 ">
+      {catagoryName.map((el) => (
+        <CatagoriesCard key={el._id} info={el} />
+      ))}
     </div>
   );
 };
--- src/Pages/Home/CatagoriesSection/CatagoriesCard.jsx
+++ src/Pages/Home/CatagoriesSection/CatagoriesCard.jsx
@@ -1,7 +1,15 @@
-export const CatagoriesCard = () => {
+export const CatagoriesCard = ({ info }) => {
   return (
-    <div className="bg-base-200 bg-gradient-to-tr from-base-100 to-base-300 shadow-lg rounded-2xl p-5">
-      CatagoriesCard
+    <div className="bg-base-200 rounded-badge bg-gradient-to-tr from-base-100 to-base-300 shadow-lg p-3 md:p-5 flex justify-around items-center">
+      <img
+        className="rounded-lg w-12 md:w-14 lg:w-16"
+        src={info.photo}
+        alt=""
+      />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="font-bold text-base md:text-xl">{info.category}</h1>
+        <button className="btn btn-sm md:btn-md">Explore Now</button>
+      </div>
     </div>
   );
 };
