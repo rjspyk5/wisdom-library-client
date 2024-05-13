45c1832951d2e863c995c38e6b5f6b7a61d861d7
rjspyk5 <rjspyk5@gmail.com>
2024-05-14 01:13:57 +0600

    filter all data and available data implement

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -7,26 +7,42 @@ export const AllBooks = () => {
   const [data, setdata] = useState([]);
   const { user } = useAuth();
   const [loading, setloading] = useState(true);
+  const [filter, setfilter] = useState("all");
 
   const axiosSequre = useAxiosSequre();
   useEffect(() => {
-    setloading(true);
-    fetchData();
-  }, []);
+    if (filter === "all") {
+      fetchData();
+    }
+    if (filter === "available") {
+      setloading(true);
+      const updateData = data.filter((el) => parseInt(el.quantity) > 0);
+      setdata(updateData);
+      setloading(false);
+    }
+  }, [filter]);
 
   const fetchData = () => {
+    setloading(true);
     user &&
-      axiosSequre.get(`/books?email=${user.email}`).then((res) => {
-        setdata(res.data);
-        setloading(false);
-      });
+      axiosSequre
+        .get(`/books?email=${user.email}`)
+        .then((res) => {
+          setdata(res.data);
+          setloading(false);
+        })
+        .catch(() => setloading(false));
   };
+
   return (
     <div>
       <div>
-        <select className="select select-bordered w-full max-w-xs">
-          <option>All Books</option>
-          <option>Available Books</option>
+        <select
+          onChange={(e) => setfilter(e.target.value)}
+          className="select select-bordered w-full max-w-xs"
+        >
+          <option value="all">All Books</option>
+          <option value="available">Available Books</option>
         </select>
       </div>
       {loading ? (
