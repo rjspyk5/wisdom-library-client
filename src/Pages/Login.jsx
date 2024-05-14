a6e39fb9e4deafcc4ed0a77d5de8c9952023f89c
rjspyk5 <rjspyk5@gmail.com>
2024-05-14 18:40:42 +0600

    simple registration page desgin complete

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -180,6 +180,15 @@ export const Login = () => {
               <h2 className="text-3xl font-bold text-center leading-tight sm:text-4xl">
                 Login now!
               </h2>
+              <p className="mt-2 text-base">
+                Don’t have an account?{" "}
+                <Link
+                  to="/register"
+                  className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
+                >
+                  Create a free account
+                </Link>
+              </p>
 
               <form onSubmit={handleLogin} className="mt-8">
                 <div className="space-y-5">
@@ -258,21 +267,24 @@ export const Login = () => {
                   </div>
                 </div>
               </form>
-              <h1 className="text-center font-semibold mt-3">
-                New Here?{" "}
-                <Link to="/register" className="text-[#48ed48]">
-                  Register now
-                </Link>
-              </h1>
-              <div className="flex items-center my-3  justify-center">
-                <div className="border-t border-1 opacity-70 flex-grow"></div>
-                <span className="mx-4  ">Or Login With</span>
-                <div className="border-t border-1 opacity-70  flex-grow"></div>
-              </div>
 
-              <div className="flex justify-center gap-4">
-                <button onClick={handleGoogleLogIn}>
-                  <FcGoogle size={30} />{" "}
+              <div className="mt-3 space-y-3">
+                <button
+                  onClick={handleGoogleLogIn}
+                  type="button"
+                  className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
+                >
+                  <div className="absolute inset-y-0 left-0 p-4">
+                    <svg
+                      className="w-6 h-6 text-rose-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                    >
+                      <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
+                    </svg>
+                  </div>
+                  Sign up with Google
                 </button>
               </div>
             </div>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -63,132 +63,9 @@ export const Register = () => {
         pauseOnHover
         theme="colored"
       />
-      <section className="bg-white dark:bg-gray-900">
-        <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
-          <form onSubmit={handleRegister} className="w-full max-w-md">
-            <h1 className="text-xl font-bold text-center">Register Now</h1>
-            <div className="relative flex items-center mt-8">
-              <span className="absolute">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
-              </span>
-
-              <input
-                name="name"
-                type="text"
-                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                placeholder="Username"
-              />
-            </div>
-
-            <div className="relative flex items-center mt-6">
-              <span className="absolute">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                  />
-                </svg>
-              </span>
-
-              <input
-                name="photo"
-                type="text"
-                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                placeholder="Photo URL"
-              />
-            </div>
-
-            <div className="relative flex items-center mt-6">
-              <span className="absolute">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-              </span>
-
-              <input
-                name="email"
-                type="email"
-                className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                placeholder="Email address"
-              />
-            </div>
-
-            <div className="relative flex items-center mt-4">
-              <span className="absolute">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                  />
-                </svg>
-              </span>
-
-              <input
-                name="pass"
-                type="password"
-                className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                placeholder="Password"
-              />
-            </div>
-
-            <div className="mt-6">
-              <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                Sign Up
-              </button>
-
-              <div className="mt-6 text-center ">
-                <Link
-                  to="/login"
-                  className="text-sm text-blue-500 hover:underline dark:text-blue-400"
-                >
-                  Already have an account?
-                </Link>
-              </div>
-            </div>
-          </form>
-        </div>
+      <section className="">
         <div>
-          <section className="bg-white">
+          <section className="">
             <div className="grid grid-cols-1 lg:grid-cols-2">
               <div className="relative flex items-end px-4 pb-10 pt-60 sm:pb-16 md:justify-center lg:pb-24 bg-gray-50 sm:px-6 lg:px-8">
                 <div className="absolute inset-0">
@@ -293,29 +170,25 @@ export const Register = () => {
                 </div>
               </div>
 
-              <div className="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
+              <div className="flex items-center justify-center px-4 py-10sm:px-6 lg:px-8 sm:py-16 lg:py-24">
                 <div className="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-                  <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
-                    Sign up to Celebration
+                  <h2 className="text-3xl font-bold leading-tight text-center sm:text-4xl">
+                    Register Now
                   </h2>
-                  <p className="mt-2 text-base text-gray-600">
+                  <p className="mt-2 text-base">
                     Already have an account?{" "}
-                    <a
-                      href="#"
-                      title=""
+                    <Link
+                      to="/login"
                       className="font-medium text-blue-600 transition-all duration-200 hover:text-blue-700 focus:text-blue-700 hover:underline"
                     >
                       Login
-                    </a>
+                    </Link>
                   </p>
 
-                  <form action="#" method="POST" className="mt-8">
+                  <form onSubmit={handleRegister} className="mt-8">
                     <div className="space-y-5">
                       <div>
-                        <label className="text-base font-medium text-gray-900">
-                          {" "}
-                          Name{" "}
-                        </label>
+                        <label className="text-base font-medium"> Name </label>
                         <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
                           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg
@@ -337,7 +210,6 @@ export const Register = () => {
                           <input
                             type="text"
                             name="name"
-                            id=""
                             placeholder="Enter your name"
                             className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
                           />
@@ -345,7 +217,7 @@ export const Register = () => {
                       </div>
 
                       <div>
-                        <label className="text-base font-medium text-gray-900">
+                        <label className="text-base font-medium ">
                           Email address
                         </label>
                         <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
@@ -369,7 +241,6 @@ export const Register = () => {
                           <input
                             type="email"
                             name="email"
-                            id=""
                             placeholder="Enter email to get started"
                             className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
                           />
@@ -377,7 +248,7 @@ export const Register = () => {
                       </div>
 
                       <div>
-                        <label className="text-base font-medium text-gray-900">
+                        <label className="text-base font-medium ">
                           Photo Url
                         </label>
                         <div className="mt-2.5 relative text-gray-400 focus-within:text-gray-600">
@@ -401,7 +272,6 @@ export const Register = () => {
                           <input
                             type="text"
                             name="photo"
-                            id=""
                             placeholder="Give Photo Url"
                             className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
                           />
@@ -409,7 +279,7 @@ export const Register = () => {
                       </div>
 
                       <div>
-                        <label className="text-base font-medium text-gray-900">
+                        <label className="text-base font-medium ">
                           {" "}
                           Password{" "}
                         </label>
@@ -434,7 +304,6 @@ export const Register = () => {
                           <input
                             type="password"
                             name="pass"
-                            id=""
                             placeholder="Enter your password"
                             className="block w-full py-4 pl-10 pr-4 text-black placeholder-gray-500 transition-all duration-200 border border-gray-200 rounded-md bg-gray-50 focus:outline-none focus:border-blue-600 focus:bg-white caret-blue-600"
                           />
@@ -451,44 +320,6 @@ export const Register = () => {
                       </div>
                     </div>
                   </form>
-
-                  <div className="mt-3 space-y-3">
-                    <button
-                      type="button"
-                      className="relative inline-flex items-center justify-center w-full px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 bg-white border-2 border-gray-200 rounded-md hover:bg-gray-100 focus:bg-gray-100 hover:text-black focus:text-black focus:outline-none"
-                    >
-                      <div className="absolute inset-y-0 left-0 p-4">
-                        <svg
-                          className="w-6 h-6 text-rose-500"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                        >
-                          <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                        </svg>
-                      </div>
-                      Sign up with Google
-                    </button>
-                  </div>
-
-                  <p className="mt-5 text-sm text-gray-600">
-                    This site is protected by reCAPTCHA and the Google{" "}
-                    <a
-                      href="#"
-                      title=""
-                      className="text-blue-600 transition-all duration-200 hover:underline hover:text-blue-700"
-                    >
-                      Privacy Policy
-                    </a>{" "}
-                    &
-                    <a
-                      href="#"
-                      title=""
-                      className="text-blue-600 transition-all duration-200 hover:underline hover:text-blue-700"
-                    >
-                      Terms of Service
-                    </a>
-                  </p>
                 </div>
               </div>
             </div>
